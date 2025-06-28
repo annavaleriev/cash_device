cc485e78747e2a5aba1faa01315961319ca60259
annavaleriev <filenko.a@gmail.com>
2025-06-28 16:11:34 +0300

    [develop] Сделана модель Item

--- cash_device/models.py
+++ cash_device/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    """Модель для товаров в магазине"""
+    title = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title
