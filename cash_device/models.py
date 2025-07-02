69165dbe435cd8dfbe9d4e4749cedeb8c5c2280e
annavaleriev <filenko.a@gmail.com>
2025-07-02 14:39:48 +0300

    [feature/models] Прописаны verbose_name для модели Item

--- cash_device/models.py
+++ cash_device/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Item(models.Model):
     """Модель для товаров в магазине"""
-    title = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    title = models.CharField(max_length=200, verbose_name="Название")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
 
     def __str__(self):
         return self.title
