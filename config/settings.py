bfb4d22c70d37c48d8ae70757f7db3fe0e54f8bb
annavaleriev <filenko.a@gmail.com>
2025-07-09 18:01:31 +0300

    [feature/connect-swagger-set-schema] Обновлены настройки проекта

--- config/settings.py
+++ config/settings.py
@@ -132,3 +132,28 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "API",
+    "VERSION": "0.0.1",
+    "SERVE_INCLUDE_SCHEMA": True,
+    "SWAGGER_UI_SETTINGS": {
+        "filter": True,
+    },
+    "COMPONENT_SPLIT_REQUEST": True,
+}
+
+COMPANY_NAME = os.getenv("COMPANY_NAME", "COMPANY_NAME")
+KKM = os.getenv("KKM", "KKM")
+INN = os.getenv("INN", "INN")
