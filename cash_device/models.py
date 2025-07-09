d199de7d0b4d2c91f1fd982c80598ef89566f9a1
annavaleriev <filenko.a@gmail.com>
2025-07-09 17:54:46 +0300

    [feature/connect-swagger-set-schema] Добавлена модель Receipt для чеков

--- cash_device/models.py
+++ cash_device/models.py
@@ -8,3 +8,19 @@ class Item(models.Model):
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+
+class Receipt(models.Model):
+    """Модель для чеков"""
+    file = models.FileField(verbose_name="Чек", upload_to="cash_device")
+
+    def __str__(self):
+        return str(self.file.name)
+
+    class Meta:
+        verbose_name = "Чек"
+        verbose_name_plural = "Чеки"
