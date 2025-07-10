5a83ee75fa19bb912e9e7c9801345addfae96d6c
annavaleriev <filenko.a@gmail.com>
2025-07-10 19:27:08 +0300

    [develop] Отформатирован код с помощью Black, отсортированы импорты с помощью isort, проверен стиль с помощью Flake8

--- cash_device/serializers.py
+++ cash_device/serializers.py
@@ -7,17 +7,3 @@ class CashMachineSerializer(serializers.Serializer):
     """Сериализатор для кассового аппарата"""
 
     items = serializers.PrimaryKeyRelatedField(queryset=Item.objects.all(), many=True)
-
-
-# Этот сериализатор проверяет входные данные: список ID товаров, которые передаёт пользователь.
-#
-# Пример запроса:
-#
-# json
-# Копировать
-# Редактировать
-# {
-#   "items": [1, 2, 3]
-# }
-
-# наверное нужен сериаолизатор для чека, но пока не реализован.
