28441f264eccdd7d90444b51ad10a65d9f6a6590
annavaleriev <filenko.a@gmail.com>
2025-07-11 17:15:23 +0300

    [develop] Отформатирован код с помощью Black, отсортированы импорты с помощью isort, проверен стиль с помощью Flake8

--- cash_device/views.py
+++ cash_device/views.py
@@ -11,27 +11,17 @@ class CashMachineViewSet(CreateModelMixin, GenericViewSet):
     """ViewSet для кассового аппарата, который принимает список товаров и возвращает QR-код с PDF-чеком."""
 
     serializer_class = CashMachineSerializer
-    queryset = (
-        Item.objects.all()
-    )  # Задаётся сериализатор и база для queryset (не используется напрямую, но нужно для DRF).
+    queryset = Item.objects.all()
 
     def create(self, request, *args, **kwargs):
         """Обрабатывает POST-запросы для создания чека."""
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(
-            raise_exception=True
-        )  # Проверяет входные данные, если не валидны, то выбрасывает исключение. Проверяет входные данные: список item.id.
+        serializer.is_valid(raise_exception=True)
 
-        pdf_generator = PDFReceiptGenerator(
-            serializer.validated_data["items"]
-        )  # Создаёт экземпляр генератора PDF-чека, передавая список товаров.
-        pdf_file = (
-            pdf_generator.generate()
-        )  # Генерирует PDF-файл чека и сохраняет его в базу данных.
+        pdf_generator = PDFReceiptGenerator(serializer.validated_data["items"])
+        pdf_file = pdf_generator.generate()
 
-        file_url = request.build_absolute_uri(
-            pdf_file.file.url
-        )  # Получает абсолютный URL PDF-файла чека, чтобы передать его в генератор QR-кода.
+        file_url = request.build_absolute_uri(pdf_file.file.url)
 
         qr_generator = QRCodeGenerator(file_url)
         qr_buffer = qr_generator.generate()
